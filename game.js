046cac6811bd28b119876ea5d0b3e9f10a8f7cf1
B1aho <wheelsurgut00@gmail.com>
2024-08-28 15:15:59 +0500

    Change function for computer choice

--- game.js
+++ game.js
@@ -1,7 +1,25 @@
 function getComputerChoice() {
-    return (Math.floor(Math.random() * 3) + 1);
+    let res = (Math.floor(Math.random() * 3) + 1);
+    switch (res) {
+        case 1: 
+        res = "ROCK";
+        break;
+
+        case 2: 
+        res = "PAPER";
+        break;
+
+        case 3:
+        res = "SCISSORS";
+        break;
+
+        default: break;
+    }
+    return res;
 }
 
 function getHumanChoice() {
     return (window.prompt("Enter the word: Rock, Pape or Scissors. Capitalization not matter!", "rock")).toUpperCase();
 }
+
+console.log(getComputerChoice()); 
