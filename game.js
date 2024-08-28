edeaca6431bb9da690aa8527c872c6938c3c92ba
B1aho <wheelsurgut00@gmail.com>
2024-08-28 15:09:32 +0500

    Add function for human choice

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
 function getComputerChoice() {
     return (Math.floor(Math.random() * 3) + 1);
 }
+
+function getHumanChoice() {
+    return (window.prompt("Enter the word: Rock, Pape or Scissors. Capitalization not matter!", "rock")).toUpperCase();
+}
